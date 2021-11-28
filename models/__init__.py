062dbec820ceac5823d3ae5b39156b91b03606f8
wooseok <qkqh8639@gmail.com>
2021-11-28 23:18:40 +0900

    Merge the Code

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,6 @@ from .models import ArcMarginModel
 from .models import ResNet
 from .models import IRBlock
 from .models import SEBlock
+
+from .wav2lip import Wav2Lip, Wav2Lip_disc_qual
+from .syncnet import SyncNet_color
